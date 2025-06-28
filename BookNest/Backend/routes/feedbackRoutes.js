c08bc7c90a1febc2ca8efee7088c3ad2ab7139e1
BODABALLA JASWANTH <jaswanthbodaballa22@gmail.com>
2025-06-28 21:01:22 +0530

    Delete BookNest directory

--- BookNest/Backend/routes/feedbackRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { submitFeedback, getBookFeedback } = require('../controllers/feedbackController');
-
-router.post('/', submitFeedback);
-router.get('/:bookId', getBookFeedback);
-
-module.exports = router;
